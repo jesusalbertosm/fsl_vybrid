b2bc5689c5cae1549bd756f401c8423af628163b
jesusalbertosm <jesus@jesus-laptop.(none)>
2013-11-07 16:40:08 -0600

    blinking_led funtion which makes the blinking led, first enabling GPIO output and them set the correspond PTB bit. Signed-off-by: jesusalbertosm <jesus@jesus-laptop.(none)>

---
+++ src/blinking_led.c
@@ -0,0 +1,53 @@
+#include "common.h"
+#include "gpio_iomux.h"
+
+
+void blinking_led_one(void){
+
+
+
+	IOMUXC->SINGLE.PTB0 = IOMUXC_PTB0_DSE(0x1)| IOMUXC_PTB0_OBE_MASK; //enable GPIO output for PTB0
+	IOMUXC->SINGLE.PTB3 = IOMUXC_PTB3_DSE(0x1)| IOMUXC_PTB3_OBE_MASK; //enable GPIO output for PTB3
+
+        while(1){
+
+                 GPIO0->PDOR&=~PIN(22);
+                 GPIO0->PDOR&=~(PIN(25)); //corresponds with PTB3
+                int a;
+                int b;
+                int counter=1000;
+                for(a=0;a<counter;a++){
+                        asm("nop");
+                        asm("nop");
+                        asm("nop");
+                   for(b=0;b<counter;b++){
+                        asm("nop");
+                        asm("nop");
+                        asm("nop");
+
+
+                       }
+
+                }
+
+
+                GPIO0->PDOR|=PIN(22);
+                GPIO0->PDOR|=PIN(25);
+
+                        for(a=0;a<counter;a++){
+                        asm("nop");
+                        asm("nop");
+                        asm("nop");
+                   for(b=0;b<counter;b++){
+                        asm("nop");
+                        asm("nop");
+                        asm("nop");
+
+
+                       }
+
+                }
+
+        }
+}
+
