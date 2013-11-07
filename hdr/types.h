c62f2403040706824092eb2bf154c6e58d182b23
jesusalbertosm <jesus@jesus-laptop.(none)>
2013-11-07 16:32:27 -0600

    types: Header where you can find all the definition types Signed-off-by: jesusalbertosm <jesus@jesus-laptop.(none)>

--- hdr/types.h
+++ hdr/types.h
@@ -10,6 +10,15 @@ typedef signed short   sint16;
 typedef unsigned int   uint32;
 typedef signed int     sint32;
 
+typedef unsigned char  uint8_t;
+typedef signed char    sint8_t;
+
+typedef unsigned short uint16_t;
+typedef signed short   sint16_t;
+
+typedef unsigned int   uint32_t;
+typedef signed int     sint32_t;
+
 #define NULL (0)
 
 #define TRUE  1
@@ -20,5 +29,18 @@ typedef signed int     sint32;
 
 #define REG_WRITE(addr, value) *(volatile unsigned int *)(addr) = value
 #define REG_READ(addr) (*(volatile unsigned int *)(addr))
+
+ #ifdef __cplusplus
+          #define   __I     volatile             /*!< Defines 'read only' permissions                 */
+        #else
+          #define   __I     volatile const       /*!< Defines 'read only' permissions                 */
+        #endif
+        #define     __O     volatile             /*!< Defines 'write only' permissions                */
+
+ #define     __IO    volatile             /*!< Defines 'read / write' permissions              */
+
+
+
+__IO uint32_t PTA6;
 
 #endif
